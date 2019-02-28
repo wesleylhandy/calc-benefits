e2f1a9469be9e68be15b6dd842d3116915994f4f
wesleylhandy <wesleylhandy@gmail.com>
2019-02-28 11:55:34 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Lost Benefits Estimator CLI
+
+This is a command-line interface for the sole purpose of estimating total lost retirement benefits if your employer quits offering matching 401k
+
+## Installation Instructions
+
+Requires:
+```
+node v8.5+
+```
+
+Currently, this CLI needs testing on a Windows-based PC.
+
+For all others:
+
+1. Fork this Repo and Clone into your development environment.
+2. `cd calc-benefits` from the folder where you cloned the repo.
+3. From the Command-line, run `npm install` to install all the dependencies into this folder
+4. Run `npm install -g` to be able to call this function anywhere
+5. Run `npm link` to connect the global installation to this directory.
+
+## Usage Instructions
+
+From the command-line, run `calc-benefits` to enter start the interface. Answer the questions you see on the screen. 
+
+* You will be prompted to enter your annual salary, the years remaining until your retirement, the previous matching percentage by your employer, and your estimated annual return from your retirment fund.
+
+
+### Command-line Options
+
+This interface allows you to skip all questions by entering the values directly on the command line when you call the function via options.
+
+**To view the complete options, run:**
+```
+calc-benefits -h
+```
+
+**Format:** _do not use quotation marks and no spaces allowed in campaign name_
+```
+calc-benefits -s <integer: salary> -y <integer: years_to_retirment> -m <float: matching_percentage> -r <float: annual_401k_return_rate>
+```
+
+**Example:**
+```
+calc-benefits -s 50000 -y 30 -m 4.0 -r 8.5
+```
---
+++ calc-benefits.js
@@ -0,0 +1,144 @@
+#! /usr/bin/env node
+
+const program = require('commander')
+const chalk = require('chalk')
+const inquirer = require('inquirer')
+
+const error = chalk.bold.redBright
+const info = chalk.bold.keyword('orange')
+
+//initialize parsing of command line arguments
+program
+    .version('0.0.1', '-v, --version')
+    .usage('[options]')
+    .option('-s, --salary <salary>', `annual salary - digits only (ie. 45000)`, parseInt)
+    .option('-y, --years <years>', `Years Left Until Retirement - whole years only (ie. 25)`, parseInt)
+    .option('-m, --match <match>', `Percentage of income usually matched - digit only (ie. 4)`, parseFloat)
+    .option('-r, --rate <rate>', `Estimated Annual Return Rate of 401k - numbers only (ie. 6.5)`, parseFloat)
+    .parse(process.argv)
+
+getMissingValues()
+    .then((nothingMissing)=>{
+        const {salary, match, rate} = program;
+        let yearCount = program.years;
+        let totalBenefits = 0.0;
+        for (yearCount; yearCount > 0; yearCount--) {
+            const yearlyBenefit = calculateTotalValue(salary, match, yearCount, rate)
+            totalBenefits += yearlyBenefit;
+          }
+        console.log('')
+        console.info(info(`With an annual salary of ${returnDollars(salary)}, your employer match would have been ${returnDollars(salary * (match / 100))} per year.`));
+        console.log('');
+        console.info(info(`With ${program.years} years remaining until retirement, and given a annual return of ${rate}% for your retirment fund, you would have earned an additional ${returnDollars(totalBenefits)} in retirement savings.`))
+        console.log('')
+    })
+    .catch(err => {
+        console.error(error(err.message))
+        console.log('')
+        console.info(info('Press CMD + C or CTRL + C to exit and start again.'))
+        console.log('')
+        setTimeout(process.exit, 2000)
+    })
+
+/**
+ * Estimates the lifetime value of a yearly contribution based on the rule of 72s
+ * @param {Number} salary 
+ * @param {Number} match 
+ * @param {Number} years 
+ * @param {Number} rate 
+ * @returns {Number} total value of the match over lifetime of benefit
+ */
+function calculateTotalValue(salary, match, years, rate){
+    const matchAmount = salary * (match / 100);
+    const yearsToDouble = 72 / rate;
+    const multiplier = years / yearsToDouble;
+
+    return matchAmount * multiplier
+}
+
+function returnDollars(value) {
+    return value.toLocaleString(undefined, {minimumFractionDigits: 2, maximiumFractionDigits: 2, style: 'currency', currency: 'USD'});
+}
+
+function getMissingValues() {
+    return new Promise((resolve, reject) => {
+        // Verify which is(are) missing and prompt user for missing value(s)
+        let questions = []
+        const {years, salary, match, rate} = program;
+        if (!salary) {
+            questions.push({
+                type: 'input',
+                name: 'salary',
+                message: 'Please enter your annual salary (Numbers Only - ie. 45000): ',
+                validate: async input => {
+                    const validated = parseInt(input)
+                    if (validated > 0) {
+                        return Promise.resolve(true)
+                    } else {
+                        return Promise.reject('Bad Input.')
+                    }
+                }
+            })
+        }
+        if (!years) {
+            questions.push({
+                type: 'input',
+                name: 'years',
+                message: 'Please enter the number of years until your retirement (Numbers Only - ie. 25): ',
+                validate: async input => {
+                    const validated = parseInt(input)
+                    if (validated > 0) {
+                        return Promise.resolve(true)
+                    } else {
+                        return Promise.reject('Bad Input.')
+                    }
+                }
+            })
+        }
+        if (!match) {
+            questions.push({
+                type: 'input',
+                name: 'match',
+                message: 'Please enter percentage your employer previously matched (Numbers Only - ie. 4.0): ',
+                validate: async input => {
+                    const validated = parseFloat(input)
+                    if (validated) {
+                        return Promise.resolve(true)
+                    } else {
+                        return Promise.reject('Bad Input.')
+                    }
+                }
+            })
+        }
+        if (!rate) {
+            questions.push({
+                type: 'input',
+                name: 'rate',
+                message: 'Please enter the percentage the expected annual return on your 401k (Numbers Only - ie. 6.0): ',
+                validate: async input => {
+                    const validated = parseFloat(input)
+                    if (validated) {
+                        return Promise.resolve(true)
+                    } else {
+                        return Promise.reject('Bad Input.')
+                    }
+                }
+            })
+        }
+        inquirer.prompt(questions).then(answers => {
+            // console.log({answers})
+            if (answers.salary) program.salary = +answers.salary
+            if (answers.years) program.years = +answers.years
+            if (answers.match) program.match = +answers.match
+            if (answers.rate) program.rate = +answers.rate
+            resolve(true)
+        }).catch(err => {
+            reject(err)
+        })
+    })
+}
+
+process.on('unhandledRejection', reason => {
+    console.error(error('Error: ' + reason))
+    setTimeout(process.exit, 2000)
+})
